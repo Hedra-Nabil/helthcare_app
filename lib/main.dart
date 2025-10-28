0ecd1f84edd1d7e21e4ddb51d479ff6679aca76d
Hedra Nabil <152221140+Hedra-Nabil@users.noreply.github.com>
2025-10-28 03:28:33 +0300

    t1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:effortless_app/screens/splash_screen.dart';
 import 'package:effortless_app/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
